e39dc618062be6bd0424634d61a2c0d5938c3cca
LiNk-NY <marcel.ramosperez@roswellpark.org>
2019-08-05 23:08:24 -0400

    add filters

--- R/HCAMatrix.R
+++ R/HCAMatrix.R
@@ -1,5 +1,6 @@
 #' @export
-.HCAMatrix <- setClass("HCAMatrix", contains = "Service")
+.HCAMatrix <- setClass("HCAMatrix", contains = "Service",
+    representation(filter = "list"))
 
 #' API Entry function for the Human Cell Atlas Matrix service
 #'
@@ -39,3 +40,36 @@ filter_detail <- function(hca, filter = "genes_detected") {
     )
 }
 
+setGeneric("filter", function(x, ...) { standardGeneric("filter") })
+
+setGeneric("filters", function(x, value) { standardGeneric("filters") })
+
+setGeneric("filters<-", function(x, value) { standardGeneric("filters<-") })
+
+#' @export
+setMethod("filter", "HCAMatrix", function(x, expr) {
+    filt <- make_filter(enquo(expr), available_filters(x))
+    filters(x) <- filt
+    x
+})
+
+#' @export
+setMethod("filters", "HCAMatrix", function(x) {
+    x@filter
+})
+
+#' @export
+setReplaceMethod("filters", c("HCAMatrix", "list"), function(x, value) {
+    current <- filters(x)
+    if (length(current))
+        slot(x, "filter") <- list( op = "and", content = list(current, value) )
+    else
+        slot(x, "filter") <- value
+    x
+})
+
+make_filter <- function(expr, available_filters) {
+    afilters <- setNames(available_filters, available_filters)
+    filterlist <- c(.oplookup, afilters)
+    rlang::eval_tidy(expr, data = filterlist)
+}
