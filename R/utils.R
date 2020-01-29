40c136b9b69d045b0675d7c5b4e6c545a4abe9a5
LiNk-NY <mramos09@gmail.com>
2020-01-29 17:27:48 -0500

    import mtxzip functionality, update all code

--- R/utils.R
+++ R/utils.R
@@ -69,3 +69,30 @@ setCache <-
     }, logical(1L))
 }
 
+.is_scalar_character <-
+    function(x, allow.na = FALSE, allow.zchar = FALSE)
+{
+    is.character(x) && length(x) == 1L && (allow.na || !is.na(x)) &&
+        (allow.zchar || nzchar(x))
+}
+
+.is_scalar_integer <-
+    function(x, allow.na = FALSE)
+{
+    is.integer(x) && length(x) == 1L && (allow.na || !is.na(x))
+}
+
+.is_scalar_logical <-
+    function(x, allow.na = FALSE)
+{
+    is.logical(x) && length(x) == 1L && (allow.na || !is.na(x))
+}
+
+.message <-
+    function(...)
+{
+    ## FIXME: use futile.logger?
+    message(...)
+    TRUE
+}
+
